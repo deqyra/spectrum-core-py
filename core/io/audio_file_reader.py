80a8f3ac73c0d38f570310f66d953039f7223609
François Brachais <francois.brachais@free.fr>
2019-03-20 13:38:25 +0100

    Reverted stuff that shouldn't be here

--- core/io/audio_file_reader.py
+++ core/io/audio_file_reader.py
@@ -1,6 +1,6 @@
 class AudioFileReader:
     def read(self):
-        raise NotImplementedError('AudioFileReader.read: abstract method was called. Do all inherited file readers override read() properly?')
+        raise NotImplementedError('AudioFileReader.read: abstract method was call. Do all inherited file readers override read() properly?')
 
     def get_sample(self):
-        raise NotImplementedError('AudioFileReader.get_sample: abstract method was called. Do all inherited file readers override get_sample() properly?')
+        raise NotImplementedError('AudioFileReader.get_sample: abstract method was call. Do all inherited file readers override get_sample() properly?')
--- core/io/audio_file_writer.py
+++
@@ -1,3 +0,0 @@
-class AudioFileWriter:
-    def write(self, filename):
-        raise NotImplementedError('AudioFileWriter.write: abstract method was called. Do all inherited file writers override write() properly?')
